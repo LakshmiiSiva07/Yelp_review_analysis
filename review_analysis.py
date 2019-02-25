9cb78fcfbfc95c13fd7252ebe6f09f6347413cc8
LakshmiiSiva07 <lak6@pdx.edu>
2019-02-24 18:32:29 -0800

    Update review_analysis.py

--- review_analysis.py
+++ review_analysis.py
@@ -12,3 +12,18 @@ yelp = pd.read_csv('yelp.csv')
 yelp.shape
 yelp.head()
 
+# Extracting Train and Test data
+X = yelp['text']
+y_multi_stars = yelp['stars']
+print(y_multi_stars[:5])
+print(y_multi_stars.value_counts())
+
+#Converting stars to demonstrate extreme polarity
+y = y_multi_stars.copy()
+for i in range(0,len(y)):
+    if(y[i] > 3):
+        y[i] = 1 # Positive
+    else:
+        y[i] = 0 # Negative
+print(y[:5])
+print(y.value_counts())
